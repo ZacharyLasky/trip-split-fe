c4d67422c9d009c5f10eff7c821e20d13c1055b6
Zach Lasky <zacharylasky123@gmail.com>
2019-11-25 13:53:23 -0500

    dashboar styles almost complete

--- ts/src/components/dashboard/Dashboard.js
+++ ts/src/components/dashboard/Dashboard.js
@@ -1,9 +1,25 @@
 import React from "react";
+import DashHeader from "../../components/header/DashHeader.js";
+
+//STYLE IMPORTS
+import DashboardStyle from "./DashboardStyle.scss";
 
 export default function Dashboard() {
   return (
-    <div className="Dash">
-      <h1>Dashboard</h1>
-    </div>
+    <>
+      <DashHeader />
+      <div className="dash-container">
+        <div className="dash">
+          <div className="hello">
+            <h1>HELLO</h1>
+            <h2>name</h2>
+          </div>
+          <div className="trips">
+            <button>NEW TRIP</button>
+            <button>ALL TRIPS</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ ts/src/components/header/DashHeader.js
@@ -0,0 +1,21 @@
+import React from "react";
+//COMPONET IMPORTS
+import HeaderModal from "../header/HeaderModal.js";
+//STYLE IMPORTS
+import HeaderStyle from "./HeaderStyle.scss";
+
+function Header() {
+  const reload = () => {
+    window.location = window.location;
+  };
+  return (
+    <div className="Header">
+      <div className="HeaderDiv">
+        <HeaderModal />
+        <h1 onClick={reload}>DASHBOARD</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
